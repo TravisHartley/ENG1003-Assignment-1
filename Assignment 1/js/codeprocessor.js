986904fd5fdfaa91dd922df10037571bf2d12858
TravisHartley <37570232+TravisHartley@users.noreply.github.com>
2018-03-20 17:17:05 +1100

    Add files via upload

---
+++ Assignment 1/js/codeprocessor.js
@@ -0,0 +1,26 @@
+/**
+ * Your header documentation here for _listen
+ *    For your reference...
+ * 		event will hold an Event object with the pixels in
+ *   	event.detail.data and the timestamp in event.timeStamp
+ */
+_listen = function(event)
+{
+	// your code here
+};
+
+/**
+ * Your header documentation here for clear
+ */
+clear = function()
+{
+	// your code here
+};
+
+/**
+ * Your header documentation here for translate
+ */
+translate = function()
+{
+	// your code here
+};
